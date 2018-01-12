a1db423ea08f8a94f403ebf606a0c8a8ad4dcfa8
tynolen <5386811+Tynolen@users.noreply.github.com>
2018-01-11 18:20:41 -0600

    Code for Problem 1

--- src/Prob1.cpp
+++ src/Prob1.cpp
@@ -2,5 +2,13 @@
 
 int main()
 {
+	std::cout<<"Calculating the sum of all multiples of 3 or 5 below 1000...\n";
 
+	int total=0;
+	for(int i=0;i<1000;i++)
+	{
+		if(i%3==0 || i%5==0) total+=i;	//brute force it by checking every number
+	}
+	std::cout<<total<<'\n';
+	return 0;
 }
